54f1552486c637e3fa16139aea26f8f697a29fb2
tcqq <1982191323@qq.com>
2018-07-09 15:20:48 +0800

    Clean up code.

--- app/src/main/java/com/example/localeswitcher/activity/LanguageSettingsActivity.kt
+++ app/src/main/java/com/example/localeswitcher/activity/LanguageSettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.example.localeswitcher.activity
 
-import android.arch.lifecycle.Lifecycle
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
@@ -10,9 +9,6 @@ import com.example.localeswitcher.adapter.items.LanguagesItem
 import com.example.localeswitcher.event.MainEvent
 import com.example.localeswitcher.manager.LanguageSettingsManager
 import com.tcqq.localeswitcher.LocaleSwitcherHelper
-import com.uber.autodispose.AutoDispose
-import com.uber.autodispose.ObservableSubscribeProxy
-import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.SelectableAdapter
 import eu.davidea.flexibleadapter.common.FlexibleItemAnimator
@@ -20,12 +16,14 @@ import eu.davidea.flexibleadapter.common.SmoothScrollLinearLayoutManager
 import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
 import io.reactivex.Observable
 import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_language_settings.*
 import org.greenrobot.eventbus.EventBus
 import timber.log.Timber
 import java.util.*
+
 
 /**
  * Language settings. Select language and switch application language locale.
@@ -37,16 +35,16 @@ class LanguageSettingsActivity : BaseActivity(),
         FlexibleAdapter.OnItemClickListener {
 
     private var activatedPosition = -1
-    private lateinit var localeList: List<Locale>
-    private lateinit var headerData: Observable<String>
-    private lateinit var listsData: Observable<String>
-    private lateinit var adapter: LanguagesAdapter
-    private val items = ArrayList<AbstractFlexibleItem<*>>()
-    private var languagesList: List<AbstractFlexibleItem<*>> = items
+    private var items = ArrayList<AbstractFlexibleItem<*>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language_settings)
+        initActionBar()
+        initRecyclerView()
+    }
+
+    private fun initActionBar() {
         setSupportActionBar(toolbar)
         val actionBar = supportActionBar
         if (actionBar != null) {
@@ -54,29 +52,6 @@ class LanguageSettingsActivity : BaseActivity(),
             actionBar.setHomeButtonEnabled(true)
             actionBar.setTitle(R.string.language_settings_title)
         }
-        text_view_selected_language.text = LanguageSettingsManager.getDisplayName(this)
-        initData()
-        Observable
-                .merge(headerData, listsData)
-                .subscribeOn(Schedulers.io())
-                .`as`<ObservableSubscribeProxy<String>>(AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(this, Lifecycle.Event.ON_DESTROY)))
-                .subscribe(object : Observer<String> {
-                    override fun onSubscribe(d: Disposable) {
-
-                    }
-
-                    override fun onNext(s: String) {
-                        items.add(LanguagesItem(items.size.toString(), s))
-                    }
-
-                    override fun onError(e: Throwable) {
-                        Timber.e(e.localizedMessage)
-                    }
-
-                    override fun onComplete() {
-                        initRecycler()
-                    }
-                })
     }
 
     override fun onBackPressed() {
@@ -96,7 +71,7 @@ class LanguageSettingsActivity : BaseActivity(),
                         .setDisplayName(getString(R.string.language_settings_device_language))
                         .apply()
             } else {
-                val locale = localeList.toTypedArray()[position - 1]
+                val locale = getLocaleList().toTypedArray()[position - 1]
                 val language = locale.language
                 val country = locale.country
                 val displayName = Locale(language, country).getDisplayName(Locale(language, country))
@@ -109,7 +84,6 @@ class LanguageSettingsActivity : BaseActivity(),
                         .apply()
             }
 
-            setActivatedPosition(position)
             LanguageSettingsManager.toggleLocale(position)
             recreate()
         }
@@ -126,32 +100,63 @@ class LanguageSettingsActivity : BaseActivity(),
         return super.onOptionsItemSelected(item)
     }
 
-    private fun initData() {
-        headerData = Observable.just(getString(R.string.language_settings_device_language))
-        localeList = Arrays.asList(
+    private fun initRecyclerView() {
+        Observable
+                .merge(getHeaderList(), getLanguagesList())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<String?> {
+                    override fun onComplete() {
+                        val adapter = LanguagesAdapter(items, this@LanguageSettingsActivity)
+                        adapter.mode = SelectableAdapter.Mode.SINGLE
+                        recycler_view.layoutManager = SmoothScrollLinearLayoutManager(this@LanguageSettingsActivity)
+                        recycler_view.adapter = adapter
+                        recycler_view.setHasFixedSize(true)
+                        recycler_view.itemAnimator = FlexibleItemAnimator()
+
+                        val position = LocaleSwitcherHelper.newInstance(this@LanguageSettingsActivity).getPosition()
+                        activatedPosition = position
+                        adapter.toggleSelection(position)
+
+                        text_view_selected_language.text = LanguageSettingsManager.getDisplayName(this@LanguageSettingsActivity)
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onNext(t: String) {
+                        items.add(LanguagesItem(items.size.toString(), t))
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Timber.e(e.localizedMessage)
+                    }
+                })
+    }
+
+
+    private fun getHeaderList(): Observable<String> {
+        return Observable.just(getString(R.string.language_settings_device_language))
+    }
+
+    private fun getLanguagesList(): Observable<String> {
+        return Observable
+                .just(getLocaleList())
+                .flatMap {
+                    Observable.fromIterable(it)
+                }
+                .map { locale ->
+                    Locale(locale.language, locale.country).getDisplayName(Locale(locale.language, locale.country))
+                }
+    }
+
+    private fun getLocaleList(): List<Locale> {
+        return Arrays.asList(
                 Locale("en"),
                 Locale("zh", "CN"),
                 Locale("ja"),
                 Locale("ko"),
                 Locale("ar"))
-        listsData = Observable
-                .just(localeList)
-                .flatMap { Observable.fromIterable(it) }
-                .map { locale -> Locale(locale.language, locale.country).getDisplayName(Locale(locale.language, locale.country)) }
-    }
-
-    private fun initRecycler() {
-        adapter = LanguagesAdapter(languagesList, this)
-        adapter.mode = SelectableAdapter.Mode.SINGLE
-        recycler_view.layoutManager = SmoothScrollLinearLayoutManager(this)
-        recycler_view.adapter = adapter
-        recycler_view.setHasFixedSize(true)
-        recycler_view.itemAnimator = FlexibleItemAnimator()
-        setActivatedPosition(LocaleSwitcherHelper.newInstance(this).getPosition())
-    }
-
-    private fun setActivatedPosition(position: Int) {
-        activatedPosition = position
-        adapter.toggleSelection(position)
     }
 }
